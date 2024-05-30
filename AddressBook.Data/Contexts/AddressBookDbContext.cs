da2bd8e0a7981f6b344821e6a1ec324b04b6c3d5
EmadERagheb <emaderagheb@gmail.com>
2024-05-30 13:36:38 +0300

    initial creation of DB

---
+++ AddressBook.Data/Contexts/AddressBookDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AddressBook.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Data.Contexts
+{
+    public class AddressBookDbContext:DbContext
+    {
+        public AddressBookDbContext(DbContextOptions<AddressBookDbContext>options):base(options)
+        {
+            
+        }
+        //public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly(),n=>n.Namespace== "AddressBook.Data.Configurations.AddressBook");
+        }
+    }
+}
--- AddressBook.Domain/Models/Person.cs
+++ AddressBook.Domain/Models/Person.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace AddressBook.Domain.Models
 {
-    public class Person
+    public class Person:BaseDomainModel
     {
+        //public string FullName { get; set; }
+
+        //public string Mobile {  get; set; }
+
+        //public string Email { get; set; }
+
+        //public string ImageUrl { get; set; }
+
+        //public DateOnly BirthDate { get; set; }
+
+        //public Address Address { get; set; }
     }
 }
