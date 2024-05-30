36ddc2a737661a9f3dfe07584c40cc5f77afd7d7
EmadERagheb <emaderagheb@gmail.com>
2024-05-30 20:10:40 +0300

    clean up program.cs

---
+++ AddressBook.API/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,27 @@
+﻿using AddressBook.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.API.Extensions
+{
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration configuration,IWebHostEnvironment environment)
+        {
+           
+            services.AddDbContext<AddressBookDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    action => action.CommandTimeout(30).EnableRetryOnFailure(maxRetryCount: 3, maxRetryDelay: TimeSpan.FromSeconds(5), errorNumbersToAdd: null));
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                options.LogTo(Console.WriteLine, LogLevel.Information);
+                if (environment.IsDevelopment())
+                {
+                    options.EnableSensitiveDataLogging();
+                    options.EnableDetailedErrors();
+                };
+            });
+            return services;
+
+        }
+    }
+}
--- AddressBook.API/Program.cs
+++ AddressBook.API/Program.cs
@@ -1,6 +1,5 @@
 
-using AddressBook.Data.Contexts;
-using Microsoft.EntityFrameworkCore;
+using AddressBook.API.Extensions;
 
 namespace AddressBook.API
 {
@@ -9,36 +8,16 @@ namespace AddressBook.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<AddressBookDbContext>(options =>
-            {
-
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-                    action => action.CommandTimeout(30).EnableRetryOnFailure(maxRetryCount: 3, maxRetryDelay: TimeSpan.FromSeconds(5), errorNumbersToAdd: null));
-                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-                options.LogTo(Console.WriteLine, LogLevel.Information);
-                if (builder.Environment.IsDevelopment())
-                {
-                    options.EnableSensitiveDataLogging();
-                    options.EnableDetailedErrors();
-                };
-
-            });
-
+            builder.Services.AddApplicationServices(builder.Configuration, builder.Environment);
+            builder.Services.AddSwaggerDocumentation();
             var app = builder.Build();
+            app.UseStaticFiles();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwaggerDocumentation();
 
             app.UseHttpsRedirection();
 
