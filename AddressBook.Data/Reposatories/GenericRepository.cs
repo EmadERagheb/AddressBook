4d61b1a68f6396252510a7760524f60ed77220e2
EmadERagheb <emaderagheb@gmail.com>
2024-05-30 21:28:44 +0300

    create Generic Repository

---
+++ AddressBook.Data/Reposatories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using AddressBook.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Data.Reposatories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public GenericRepository()
+        {
+            
+        }
+        public Task<TResult> GetAsync<TResult>(Expression<Func<T, bool>> filter, params string[] properties)
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Exists(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TResult>> GetAllAsync<TResult>(int pageNmuber, int pageSize, Expression<Func<T, bool>?> filter = null, Expression<Func<T, object>?> order = null, Expression<Func<T, object>?> orderDesc = null, params string[] properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TResult>> GetAllAsync<TResult>(Expression<Func<T, bool>> filter, params string[] properties)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<int> GetCountAsync(Expression<Func<T, bool>?> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AddressBook.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Domain.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<TResult> GetAsync<TResult>(Expression<Func<T, bool>> filter, params string[] properties);
+
+        Task<List<TResult>> GetAllAsync<TResult>(int pageNmuber,
+            int pageSize, Expression<Func<T, bool>?> filter = null, Expression<Func<T, object>?> order = null, Expression<Func<T, object>?> orderDesc = null, params string[] properties);
+
+        Task<List<TResult>> GetAllAsync<TResult>(Expression<Func<T, bool>> filter, params string[] properties);
+        Task<int> GetCountAsync(Expression<Func<T, bool>?> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        Task<bool> Exists(Expression<Func<T, bool>> filter);
+    }
+}
