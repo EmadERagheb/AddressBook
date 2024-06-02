297ec07d7b6d709fe947dd502ce8d84984e14e01
EmadERagheb <emaderagheb@gmail.com>
2024-06-02 12:17:43 +0300

    create login form

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TextInputComponent } from './components/text-input/text-input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TextInputComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports:[
+    TextInputComponent,
+    ReactiveFormsModule
+
   ]
 })
 export class SharedModule { }
